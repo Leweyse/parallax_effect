ff7036a932b575c193a34935d169e9d98f8ca9cb
Leweyse <darylcastro67@gmail.com>
2021-11-18 12:30:48 +0100

    implemented logic for character

--- js/index.js
+++ js/index.js
@@ -4,7 +4,11 @@ const main = document.getElementById('main');
 const sections = document.querySelectorAll('.sections');
 const arr = [...document.getElementsByClassName('cloud')];
 
+const arr = [...document.querySelectorAll('.cloud')];
 const clouds = getRandom(arr, arr.length);
+
+const standing = document.getElementById('standing');
+const running = document.getElementById('running');
 
 let position = {
     x: 0,
@@ -18,15 +22,35 @@ window.addEventListener('wheel', function (event) {
     // Disable page scrolling, modes[event.deltaMode]=='page'
     if (event.deltaMode > 1) return false;
 
-    position.y = position.y + (deltabs * 10);
+    if (xEventValue === -1) running.style.transform = 'scaleX(-1)';
+    else running.style.transform = 'scaleX(1)';
+
+    position.x = position.x + (xEventValue * 10);
 
     clouds.forEach((cloud, idx) => {
-        cloud.style.setProperty('transform', "translate(" + Math.round(position.y) * ((idx % (clouds.length / 15)) + 1) + "px," + position.x + "px)")
+        cloud.style.setProperty('transform', "translate(" + Math.round(position.x) * (idx % (clouds.length / 10)) + "px," + 0 + "px)")
     });
 
     sections.forEach((section) => {
-        section.style.setProperty('transform', "translate(" + Math.round(position.y) + "px," + position.x + "px)")
+        section.style.setProperty('transform', "translate(" + Math.round(position.x) + "px," + 0 + "px)")
     })
-
-    return false;
 })
+
+const render = () => {
+    if (movement.length === 1) {
+        standing.style.setProperty('opacity', '1')
+        running.style.setProperty('opacity', '0')
+    } else {
+        standing.style.setProperty('opacity', '0')
+        running.style.setProperty('opacity', '1')
+    }
+
+    movement = ['running'];
+
+    setTimeout(() => {
+        render();
+    }, 100);
+}
+
+render();
+
